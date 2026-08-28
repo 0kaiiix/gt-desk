643c6700fd87ac71ae5d4b9de974caca6bb0651d
kaiiix <118248699+0kaiiix@users.noreply.github.com>
2026-08-28 10:57:36 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import {
   fetchAllPedestalsFromFirestore,
 } from './lib/firebase';
 
-const STORAGE_KEY = 'OFFICE_PEDESTALS_CUSTOMER_MAP_V34_UPDATE_GT1636';
+const STORAGE_KEY = 'OFFICE_PEDESTALS_CUSTOMER_MAP_V35_MANUAL_STATUS_ALLOWED';
 
 export default function App() {
   const [pedestals, setPedestals] = useState<Pedestal[]>(() => {
